7c357d0549bb54d8301568986ac8d2aed510c774
Igor Volnyi <igorvolnyi@gmail.com>
2020-11-25 19:06:38 +0500

    Fix bad definition of NotFoundError for TS
    Issue \#31

--- index.d.ts
+++ index.d.ts
@@ -18,7 +18,7 @@ declare module "join-js" {
       columnPrefix?: string;
     }[];
   };
-  declare function NotFoundError(message?: string): void;
+  export type NotFoundError = (message?: string) => void;
   const JoinJs: {
     map(resultSet: any[], maps: ResultMap[], mapId: string, columnPrefix?: string): any;
     mapOne(resultSet: any[], maps: ResultMap[], mapId: string, columnPrefix?: string, isRequired?: boolean): any;
